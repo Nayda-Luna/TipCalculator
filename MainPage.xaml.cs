4a9b1736ea1cc954ddcd387bba15967b2f05309b
Nayda <naluna909@gmail.com>
2024-08-23 21:40:47 -0300

    eu criei o projeto

---
+++ MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+       
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void setLowTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void setHighTip_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
